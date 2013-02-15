93dd4e56ac53e20a7700317737aafd89509b6ba6
nardholio <nardholio@gmail.com>
2013-02-15 13:24:02 -0500

    update build fingerprint for 4.2.2

--- cm.mk
+++ cm.mk
@@ -9,7 +9,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=apexqtmo TARGET_DEVICE=apexqtmo BUILD_FINGERPRINT="apexqtmo-user 4.2.1 JOP40G T699UVDLJA release-keys" PRIVATE_BUILD_DESC="samsung/apexqtmo/apexqtmo:4.2.1/JOP40G/T699UVDLJA:user/release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=apexqtmo TARGET_DEVICE=apexqtmo BUILD_FINGERPRINT="apexqtmo-user 4.2.2 JDQ39 T699UVDLJA release-keys" PRIVATE_BUILD_DESC="samsung/apexqtmo/apexqtmo:4.2.2/JDQ39/T699UVDLJA:user/release-keys"
 
 PRODUCT_NAME := cm_apexqtmo
 PRODUCT_DEVICE := apexqtmo
