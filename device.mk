4274992f2158634e27565bc0dfaebaf2a99d97db
David Daynard <nardholio@gmail.com>
2013-06-11 20:52:51 -0400

    Fix wifi scan support for battery life

--- device.mk
+++ device.mk
@@ -118,7 +118,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.svlte1x=true \
     persist.radio.no_wait_for_card=1 \
     keyguard.no_require_sim=true \
-    media.aac_51_output_enabled=true
+    media.aac_51_output_enabled=true \
+    wifi.supplicant_scan_interval = 15
 
 # common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
